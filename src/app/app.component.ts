54a55b52af277e523bc58d3a59f14486635b784b
root <root@DESKTOP-K8JHRR3>
2024-10-08 19:22:45 -0300

    funcional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListComponent } from '../lista/lista.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, ListComponent],
+  template: `<list-root />`
 })
 export class AppComponent {
   title = 'minha-aplicacao';
--- src/lista/lista.component.ts
+++ src/lista/lista.component.ts
@@ -1,13 +1,79 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+type Lista = {
+  compra: String,
+  comprado: boolean
+}
 
 @Component({
-  selector: 'app-root',
+  selector: 'list-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, FormsModule],
+  template: `
+    <div class="container">
+      <input class="input-task" placeholder="Nome do item" [(ngModel)]="nomeDaCompra">
+      <button class="button-add-task" (click)="adicionarNovaCompra(nomeDaCompra)">Adicionar</button>
+      <ul class="list-tasks">
+        @for (item of listaDeItens; track $index) {
+          @if (item.comprado == false) {
+            <li>{{item.compra}}</li>
+            @if (!isEditing) {
+              <button (click)="editar($index)">Editar</button>
+            } @else {
+              <input class="input-task" placeholder="Nome do item" [(ngModel)]="editCompra">
+              <button (click)="editar($index, editCompra)">Confirmar</button>
+            }
+            <button (click)="comprarItem($index)">Comprar</button>
+            <button (click)="deletarItem($index)">Deletar</button>
+          }
+        }
+      </ul>
+      Itens Comprados:
+      <ul class="list-tasks">
+        @for (item of listaDeItens; track $index) {
+          @if (item.comprado == true) {
+            <li>{{item.compra}}</li>
+            @if (!isEditing) {
+              <button (click)="editar($index)">Editar</button>
+            } @else {
+              <input class="input-task" placeholder="Nome do item" [(ngModel)]="editCompra">
+              <button (click)="editar($index, editCompra)">Confirmar</button>
+            }
+            <button (click)="comprarItem($index)">Comprado</button>
+            <button (click)="deletarItem($index)">Deletar</button>
+          }
+        }
+      </ul>
+    </div>
+  `,
 })
-export class AppComponent {
-  title = 'minha-aplicacao';
+export class ListComponent {
+  listaDeItens: Lista[] = [];
+  nomeDaCompra = '';
+  editCompra = '';
+  isEditing = false;
+  
+  adicionarNovaCompra(name: String) {
+    this.listaDeItens.push({
+      compra: name,
+      comprado: false,
+    })
+    this.nomeDaCompra = '';
+  }
+
+  comprarItem(posicao: number) {
+    this.listaDeItens[posicao].comprado = !this.listaDeItens[posicao].comprado
+  }
+  
+  deletarItem(posicao: number) {
+    this.listaDeItens.splice(posicao, 1)
+  }
+
+  editar(posicao: number, name: String = '') {
+    this.isEditing = !this.isEditing;
+    this.listaDeItens[posicao].compra = name;
+    this.nomeDaCompra = '';
+  }
 }
